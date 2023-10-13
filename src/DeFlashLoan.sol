09d1edd02278cd58797ecd109ccc658374eec0b9
Wim Van Renterghem <git@vrwim.com>
2023-10-13 14:09:32 +0200

    Written some (probably unsafe) code

--- src/DeFlashLoan.sol
+++ src/DeFlashLoan.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "openzeppelin-contracts/token/ERC1155/ERC1155.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "openzeppelin-contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "./IERC3156FlashLender.sol";
 
 contract DeFlashLoan is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply, IERC3156FlashLender {
@@ -13,49 +13,143 @@ contract DeFlashLoan is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply, IERC31
         uint thisFeeAmount;
         /// @dev The amount of rewards in the pool that haven't been distributed at this fee level
         uint rewardsToDistribute;
+        /// @dev The amount of rewards accumulated per token, this is needed to give each user their fair share
+        uint rewardPerToken;
 
         // This becomes a doubly linked list
         uint previousFee;
         uint nextFee;
     }
 
+    /// @dev A struct to store user info
     struct UserInfo {
         uint amount;
         uint rewardDebt;
     }
 
-    // Token address => lowest fee amount (start of doubly linked list)
+    /// @dev Token address => lowest fee amount (start of doubly linked list)
     mapping(address => uint) public lowestFeeAmount;
 
-    // Token address => total available tokens (to cache this commonly requested value)
+    /// @dev Token address => total available tokens (to cache this commonly requested value)
     mapping(address => uint) public totalAvailable;
 
-    // Token address => fee amount => fee pool
+    /// @dev Token address => fee amount => fee pool
     mapping(address => mapping(uint => Pool)) public pools;
 
-    // User address => token address => fee level => user info
+    /// @dev User address => token address => fee level => user info
     mapping(address => mapping(address => mapping(uint => UserInfo))) public userInfo;
 
     constructor(address initialOwner) ERC1155("") Ownable(initialOwner) { }
 
-    // TODO: refactor to ERC20Receiver or something
     function deposit(address token, uint amount, uint fee) external {
         // Does fee exist?
-        // Yes: add amount to mapping
-        // No: Create mapping, change previousFee and nextFee of previous and next fee
+        if(pools[token][fee].thisFeeAmount > 0) {
+            // Yes: add amount to mapping
+            pools[token][fee].thisFeeAmount += amount;
+        } else {
+            // No: Create mapping, change previousFee and nextFee of previous and next fee
+            // Search in pools[token] to find a place to insert in the linked list
+            // Start with the lowest fee in the pool
+            uint previousFee = lowestFeeAmount[token];
+            // If the lowest fee is higher than the fee we want to insert, insert it at the start of the list
+            if(previousFee < fee) {
+                // Insert at start of list
+                pools[token][fee] = Pool({
+                    thisFeeAmount: amount,
+                    rewardsToDistribute: 0,
+                    rewardPerToken: 0,
+                    previousFee: 0,
+                    nextFee: previousFee
+                });
+                // Change previousFee of next fee
+                pools[token][previousFee].previousFee = fee;
+                // Change lowestFeeAmount
+                lowestFeeAmount[token] = fee;
+            } else {
+                // Search for the right place to insert, after this while loop previousFee will be the fee level after the one we want to insert
+                while(previousFee < fee) {
+                    previousFee = pools[token][previousFee].nextFee;
+                }
+
+                // Get next fee
+                uint nextFee = pools[token][previousFee].nextFee;
+
+                // Change nextFee of previous fee
+                pools[token][previousFee].nextFee = fee;
+
+                // Change previousFee of next fee
+                pools[token][nextFee].previousFee = fee;
+
+                // Insert new fee level
+                pools[token][fee] = Pool({
+                    thisFeeAmount: amount,
+                    rewardsToDistribute: 0,
+                    rewardPerToken: 0,
+                    previousFee: previousFee,
+                    nextFee: nextFee
+                });
+            }
+        }
         // Give liquidity tokens to user
+        _mint(msg.sender, token, amount, 0x00);
         // Save user info
+        userInfo[msg.sender][token][fee].amount += amount;
+        // Add to total available
+        totalAvailable[token] += amount;
+
+        // Take ERC20 tokens from user
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
     }
 
     function withdraw(address token, uint amount, uint fee) external {
+        // Give user rewards
+        distributeRewards(token, fee);
+
         // Subtract amount from mapping
+        pools[token][fee].thisFeeAmount -= amount;
         // Is thisFeeAmount 0?
-        // Yes: Delete mapping, change previousFee and nextFee of previous and next fee
+        if(pools[token][fee].thisFeeAmount == 0) {
+            // Yes: Delete mapping, change previousFee and nextFee of previous and next fee
+            // Check if this is lowest fee
+            if(lowestFeeAmount[token] == fee) {
+                // Yes: Change lowestFeeAmount
+                lowestFeeAmount[token] = pools[token][fee].nextFee;
+                // Change previousFee to 0
+                pools[token][lowestFeeAmount[token]].previousFee = 0;
+                // Delete to save gas
+                delete pools[token][fee];
+            } else {
+                // TODO: what if it's the last one?
+                // No: Change nextFee of previous fee and previousFee of next fee
+                uint nextFee = pools[token][fee].nextFee;
+                uint previousFee = pools[token][fee].previousFee;
+
+                pools[token][previousFee].nextFee = nextFee;
+                // Change previousFee of next fee
+                pools[token][nextFee].previousFee = previousFee;
+                // Delete to save gas
+                delete pools[token][fee];
+            }
+        }
+        // Burn liquidity tokens
+        _burn(msg.sender, token, amount);
+        // Subtract from total available
+        totalAvailable[token] -= amount;
+
+        // Give ERC20 tokens to user
+        IERC20(token).transfer(msg.sender, amount);
     }
 
-    function distributeRewards(address token, uint feeLevel) external {
-        // Check if user has rewards
+    function distributeRewards(address token, uint feeLevel) external returns (uint){
+        UserInfo storage user = userInfo[msg.sender][token][feeLevel];
+        Pool storage pool = pools[token][feeLevel];
+        // Check if user has rewards for token and fee level
+        uint rewards = user.amount * (pool.rewardPerToken - user.rewardDebt);
+        require(rewards > 0, "DeFlashLoan: No rewards to distribute");
         // Yes: transfer rewards to user
+        IERC20(token).transfer(msg.sender, rewards);
+        // Update user info
+        user.rewardDebt = pool.rewardPerToken;
     }
 
     // ERC1155
@@ -112,19 +206,19 @@ contract DeFlashLoan is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply, IERC31
         // Pay back flash loan + commission
         // Distribute commission
 
-        uint256 fee = _flashFee(token, amount);
-        // TODO: check if this is correct
-        IERC20(token).transfer(address(receiver), amount);
-        require(
-            receiver.onFlashLoan(msg.sender, token, amount, fee, data) == CALLBACK_SUCCESS,
-            "FlashMinter: Callback failed"
-        );
-        uint256 _allowance = allowance(address(receiver), address(this));
-        require(
-            _allowance >= (amount + fee),
-            "FlashMinter: Repay not approved"
-        );
-        _approve(address(receiver), address(this), _allowance - (amount + fee));
+        // uint256 fee = _flashFee(token, amount);
+        // // TODO: check if this is correct
+        // IERC20(token).transfer(address(receiver), amount);
+        // require(
+        //     receiver.onFlashLoan(msg.sender, token, amount, fee, data) == CALLBACK_SUCCESS,
+        //     "FlashMinter: Callback failed"
+        // );
+        // uint256 _allowance = allowance(address(receiver), address(this));
+        // require(
+        //     _allowance >= (amount + fee),
+        //     "FlashMinter: Repay not approved"
+        // );
+        // _approve(address(receiver), address(this), _allowance - (amount + fee));
         return true;
     }
 
--- test/DeFlashLoan.t.sol
+++ test/DeFlashLoan.t.sol
@@ -4,10 +4,56 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {DeFlashLoan} from "../src/DeFlashLoan.sol";
 
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
 contract DeFlashLoanTest is Test {
     DeFlashLoan public myContract;
+    DummyERC20 public dummyToken;
 
     function setUp() public {
         myContract = new DeFlashLoan();
+        dummyToken = new DummyERC20();
+    }
+
+    function testDeposit() public {
+        myContract.deposit(address(dummyToken), 100, 10);
+        assertEq(myContract.totalAvailable(address(dummyToken)), 100);
+    }
+
+    function testDeposit() public {
+        myContract.deposit(address(dummyToken), 100, 10);
+        myContract.deposit(address(dummyToken), 100, 20);
+        assertEq(myContract.totalAvailable(address(dummyToken)), 200);
+    }
+
+    function testPartialDeposit() public {
+        myContract.deposit(address(dummyToken), 100, 10);
+        myContract.deposit(address(dummyToken), 50, 10);
+        assertEq(myContract.totalAvailable(address(dummyToken)), 150);
+    }
+
+    function testWithdraw() public {
+        myContract.deposit(address(dummyToken), 100, 10);
+        myContract.withdraw(address(dummyToken), 100, 10);
+        assertEq(myContract.totalAvailable(address(dummyToken)), 0);
+    }
+
+    function testPartialWithdraw() public {
+        myContract.deposit(address(dummyToken), 100, 10);
+        myContract.withdraw(address(dummyToken), 50, 10);
+        assertEq(myContract.totalAvailable(address(dummyToken)), 50);
+    }
+}
+
+/// @dev A mock ERC20 token that always succeeds transfers
+contract DummyERC20 is ERC20 {
+    constructor() ERC20("Dummy", "DUM") { }
+
+    // Fake transfer method that always succeeds
+    function transfer(address, uint) public override returns (bool) {
+        return true;
+    }
+    function transferFrom(address, address, uint) public override returns (bool) {
+        return true;
     }
 }
